a1d6594a5da58cd567e7bb568d6088e4e4b1990a
wishtree-rpatil <rahul.patil@wishtreetech.com>
2022-12-09 16:54:30 +0530

    resolved conflicts

--- frontend/wordle-front/src/wordle/CustomInput.js
+++ frontend/wordle-front/src/wordle/CustomInput.js
@@ -8,30 +8,14 @@ const CustomIput = styled(TextField)({
         borderWidth:"1px",
 
       },
-<<<<<<< HEAD
-      '&:hover fieldset': {
-        borderColor: 'rgba(0, 0, 0, 0.23)',
-=======
       '& input':{
          padding: "12.5px 14px",
->>>>>>> f27de20b46f2646abceb9769dac79969a82bf6b7
       },
       
       '&.Mui-focused fieldset': {
-<<<<<<< HEAD
-        borderColor: 'rgba(0, 0, 0, 0.23)',
-      },
-      '&.Mui-disabled fieldset': {
-        borderColor: 'rgba(0, 0, 0, 0.23)',
-        cursor: "not-allowed"
-      },
-      '&.Mui-error fieldset': {
-        borderColor: '#f44336',
-=======
         borderColor: '#d3d6da',
         borderWidth:"1px"
 
->>>>>>> f27de20b46f2646abceb9769dac79969a82bf6b7
       },
       
     },
