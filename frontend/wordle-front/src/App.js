9a117739c87173c147d06fde51b9831504f40a49
wishtree-mjaveri <madhav.javeri@wishtreetech.com>
2022-12-09 12:38:46 +0530

    Added dailog box for wordle subscription

--- frontend/wordle-front/src/App.js
+++ frontend/wordle-front/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Wordle from "./wordle/Wordle";
+import Form from "./wordle/Form";
 
 function App() {
     return (
         <div>
             <Wordle />
+            <Form />
         </div>
     );
 }
