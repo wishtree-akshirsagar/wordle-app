69c362277ae8f76e38f381e64d35778701e8810d
snehakasar <sneha.kasar@wishtreetech.com>
2022-12-09 12:44:03 +0530

    added header

---
+++ frontend/wordle-front/src/wordle/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import  Typography   from '@mui/material/Typography'
+import './header.css'
+
+const Header = () => {
+    return (
+    <div className="wordle-header">
+        <Typography className="header-heading">
+        Wordle
+        </Typography>
+        </div>
+    )
+};
+
+export default Header;
