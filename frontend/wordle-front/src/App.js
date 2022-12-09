0ce2ca59b7cc85189eb2f341c5882f7297423e52
wishtree-mjaveri <madhav.javeri@wishtreetech.com>
2022-12-09 12:15:33 +0530

    react app created

---
+++ frontend/wordle-front/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Wordle from "./wordle/Wordle";
+
+function App() {
+    return (
+        <div>
+            <Wordle />
+        </div>
+    );
+}
+
+export default App;
